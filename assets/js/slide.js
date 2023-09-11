9ba5384616ff0123f8573d4bf6da1335aa1e7380
30th-nodl <136173116+30th-nodl@users.noreply.github.com>
2023-09-11 22:48:02 +0900

    Add files via upload

--- assets/js/slide.js
+++ assets/js/slide.js
@@ -1,94 +1,72 @@
-$(".slider").each(function () {
-  var $this = $(this);
-  var $group = $this.find(".slide_group");
-  var $slides = $this.find(".slide");
-  var bulletArray = [];
-  var currentIndex = 0;
-  var timeout;
+$(function () {
+  var slider01 = new Swiper(".gallery-slider-01", {
+    autoplay: {
+      delay: 4000,
+    },
 
-  function move(newIndex) {
-    var animateLeft, slideLeft;
-
-    advance();
-
-    if ($group.is(":animated") || currentIndex === newIndex) {
-      return;
-    }
-
-    bulletArray[currentIndex].removeClass("active");
-    bulletArray[newIndex].addClass("active");
-
-    if (newIndex > currentIndex) {
-      slideLeft = "100%";
-      animateLeft = "-100%";
-    } else {
-      slideLeft = "-100%";
-      animateLeft = "100%";
-    }
-
-    $slides.eq(newIndex).css({
-      display: "block",
-      left: slideLeft,
-    });
-    $group.animate(
-      {
-        left: animateLeft,
-      },
-      function () {
-        $slides.eq(currentIndex).css({
-          display: "none",
-        });
-        $slides.eq(newIndex).css({
-          left: 0,
-        });
-        $group.css({
-          left: 0,
-        });
-        currentIndex = newIndex;
-      }
-    );
-  }
-
-  function advance() {
-    clearTimeout(timeout);
-    timeout = setTimeout(function () {
-      if (currentIndex < $slides.length - 1) {
-        move(currentIndex + 1);
-      } else {
-        move(0);
-      }
-    }, 8000);
-  }
-
-  $(".next_btn").on("click", function () {
-    if (currentIndex < $slides.length - 1) {
-      move(currentIndex + 1);
-    } else {
-      move(0);
-    }
+    slidesPerView: 1,
+    centeredSlides: true,
+    loop: true,
+    loopedSlides: 29, //슬라이드 수와 동일한 값을 지정
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
   });
 
-  $(".previous_btn").on("click", function () {
-    if (currentIndex !== 0) {
-      move(currentIndex - 1);
-    } else {
-      move(3);
-    }
+  var thumbs01 = new Swiper(".gallery-thumbs-01", {
+    slidesPerView: "auto",
+    spaceBetween: 12,
+    centeredSlides: true,
+    loop: true,
+    slideToClickedSlide: true,
   });
 
-  $.each($slides, function (index) {
-    var $button = $('<a class="slide_btn">&bull;</a>');
+  slider01.controller.control = thumbs01;
+  thumbs01.controller.control = slider01;
+});
 
-    if (index === currentIndex) {
-      $button.addClass("active");
-    }
-    $button
-      .on("click", function () {
-        move(index);
-      })
-      .appendTo(".slide_buttons");
-    bulletArray.push($button);
+$(function () {
+  var slider02 = new Swiper(".gallery-slider-02", {
+    autoplay: {
+      delay: 8000,
+    },
+
+    slidesPerView: 1,
+    centeredSlides: true,
+    loop: true,
+    loopedSlides: 4, //슬라이드 수와 동일한 값을 지정
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+});
+
+$(function () {
+  var slider03 = new Swiper(".gallery-slider-03", {
+    autoplay: {
+      delay: 4000,
+    },
+
+    slidesPerView: 1,
+    centeredSlides: true,
+    loop: true,
+    loopedSlides: 29, //슬라이드 수와 동일한 값을 지정
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
   });
 
-  advance();
+  var thumbs03 = new Swiper(".gallery-thumbs-03", {
+    slidesPerView: "auto",
+    spaceBetween: 12,
+    centeredSlides: true,
+    loop: true,
+    slideToClickedSlide: true,
+  });
+
+  slider03.controller.control = thumbs03;
+  thumbs03.controller.control = slider03;
 });

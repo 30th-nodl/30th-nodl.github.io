8aed8af179e584855a14a8c09286b51f3c3d3956
30th-nodl <136173116+30th-nodl@users.noreply.github.com>
2023-12-29 18:01:18 +0900

    Add files via upload

--- assets/js/slide.js
+++ assets/js/slide.js
@@ -7,7 +7,7 @@ $(function () {
     slidesPerView: 1,
     centeredSlides: true,
     loop: true,
-    loopedSlides: 29, //슬라이드 수와 동일한 값을 지정
+    loopedSlides: 54, //슬라이드 수와 동일한 값을 지정
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
@@ -16,7 +16,7 @@ $(function () {
 
   var thumbs01 = new Swiper(".gallery-thumbs-01", {
     slidesPerView: "auto",
-    spaceBetween: 12,
+    spaceBetween: 8,
     centeredSlides: true,
     loop: true,
     slideToClickedSlide: true,
@@ -29,18 +29,29 @@ $(function () {
 $(function () {
   var slider02 = new Swiper(".gallery-slider-02", {
     autoplay: {
-      delay: 8000,
+      delay: 4000,
     },
 
     slidesPerView: 1,
     centeredSlides: true,
     loop: true,
-    loopedSlides: 4, //슬라이드 수와 동일한 값을 지정
+    loopedSlides: 190, //슬라이드 수와 동일한 값을 지정
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
   });
+
+  var thumbs02 = new Swiper(".gallery-thumbs-02", {
+    slidesPerView: "auto",
+    spaceBetween: 8,
+    centeredSlides: true,
+    loop: true,
+    slideToClickedSlide: true,
+  });
+
+  slider02.controller.control = thumbs02;
+  thumbs02.controller.control = slider02;
 });
 
 $(function () {
@@ -52,10 +63,51 @@ $(function () {
     slidesPerView: 1,
     centeredSlides: true,
     loop: true,
-    loopedSlides: 29, //슬라이드 수와 동일한 값을 지정
+    loopedSlides: 35, //슬라이드 수와 동일한 값을 지정
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
   });
+
+  var thumbs03 = new Swiper(".gallery-thumbs-03", {
+    slidesPerView: "auto",
+    spaceBetween: 8,
+    centeredSlides: true,
+    loop: true,
+    slideToClickedSlide: true,
+  });
+
+  slider03.controller.control = thumbs03;
+  thumbs03.controller.control = slider03;
+});
+
+
+
+$(function () {
+  var slider04 = new Swiper(".gallery-slider-04", {
+    autoplay: {
+      delay: 4000,
+    },
+
+    slidesPerView: 1,
+    centeredSlides: true,
+    loop: true,
+    loopedSlides: 48, //슬라이드 수와 동일한 값을 지정
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  var thumbs04 = new Swiper(".gallery-thumbs-04", {
+    slidesPerView: "auto",
+    spaceBetween: 8,
+    centeredSlides: true,
+    loop: true,
+    slideToClickedSlide: true,
+  });
+
+  slider04.controller.control = thumbs04;
+  thumbs04.controller.control = slider04;
 });
